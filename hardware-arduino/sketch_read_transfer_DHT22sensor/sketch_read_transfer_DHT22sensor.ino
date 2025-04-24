a0ff350b7f8d35593f207b77777fdda184359425
windyinmay <quynhanhmay.butter@gmail.com>
2025-04-24 12:30:26 +0300

    tested with iphone but no result on phone

--- hardware-arduino/sketch_read_transfer_DHT22sensor/sketch_read_transfer_DHT22sensor.ino
+++ hardware-arduino/sketch_read_transfer_DHT22sensor/sketch_read_transfer_DHT22sensor.ino
@@ -65,3 +65,71 @@ void loop(void) {
 
   delay(5000); // wait 5 seconds before sending again
 }
+// #include <SPI.h>
+// #include <Adafruit_PN532.h>
+// #include <Adafruit_Sensor.h>
+// #include <DHT.h>
+
+// #define DHTPIN 2       // DHT22 data pin
+// #define DHTTYPE DHT22
+// DHT dht(DHTPIN, DHTTYPE);
+
+// // PN532 SPI pins for Arduino Uno
+// #define PN532_SCK  (13)
+// #define PN532_MOSI (11)
+// #define PN532_SS   (10)
+// #define PN532_MISO (12)
+// Adafruit_PN532 nfc(PN532_SS);
+
+// void setup() {
+//   Serial.begin(115200);
+//   dht.begin();
+
+//   nfc.begin();
+//   uint32_t versiondata = nfc.getFirmwareVersion();
+//   if (!versiondata) {
+//     Serial.println("Didn't find PN532 board");
+//     while (1);
+//   }
+
+//   nfc.SAMConfig();
+//   Serial.println("PN532 NFC ready. Writing DHT22 data to NFC...");
+// }
+
+// void loop() {
+//   float h = dht.readHumidity();
+//   float t = dht.readTemperature();
+
+//   if (isnan(h) || isnan(t)) {
+//     Serial.println("Failed to read from DHT sensor!");
+//     delay(2000);
+//     return;
+//   }
+
+//   // Format message
+//   String message = "T:" + String(t, 1) + ",H:" + String(h, 1);
+//   Serial.print("Writing to NFC: ");
+//   Serial.println(message);
+
+//   // Convert to char buffer
+//   char dataBuffer[32];
+//   message.toCharArray(dataBuffer, 32);
+
+//   // Write to tag using NTAG pages (each page is 4 bytes)
+//   for (int i = 0; i < 8; i++) {
+//     uint8_t pageData[4] = {0, 0, 0, 0};
+//     for (int j = 0; j < 4; j++) {
+//       int index = i * 4 + j;
+//       if (index < strlen(dataBuffer)) {
+//         pageData[j] = dataBuffer[index];
+//       }
+//     }
+//     if (!nfc.ntag2xx_WritePage(4 + i, pageData)) {
+//       Serial.print("Failed to write page "); Serial.println(4 + i);
+//     }
+//   }
+
+//   Serial.println("Data written to NFC moduleS!");
+//   delay(5000); // Write every 5 seconds
+// }
+
